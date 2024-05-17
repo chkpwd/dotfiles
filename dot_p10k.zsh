b6580b921639e85f02d6e710e5a6df86ea22cd81
Bryan Jones <unix.chkpwd@gmail.com>
2024-05-17 11:52:11 -0400

    new features

--- dot_p10k.zsh
+++ dot_p10k.zsh
@@ -62,9 +62,8 @@
   # Right prompt segments.
   typeset -g POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(
     # =========================[ Line #1 ]=========================
-    # command_execution_time  # previous command duration
-    # virtualenv              # python virtual environment
-    # context                 # user@host
+    command_execution_time    # previous command duration
+    context                   # user@host
     time                      # current time
     # =========================[ Line #2 ]=========================
     newline                   # \n
