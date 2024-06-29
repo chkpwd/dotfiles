ce24967d3ba26671a005f407caa4a14da0c4b496
Bryan Jones <unix.chkpwd@gmail.com>
2024-06-29 14:16:58 -0400

    fixed issues with coding

--- dot_config/nvim/lua/chkpwd/plugins/mason.lua
+++ dot_config/nvim/lua/chkpwd/plugins/mason.lua
@@ -1,53 +1,48 @@
 return {
-  "williamboman/mason.nvim",
-  dependencies = {
-    "williamboman/mason-lspconfig.nvim",
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-  },
-  config = function()
-    -- import mason
-    local mason = require("mason")
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig.nvim",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+	},
+	config = function()
+		-- import mason
+		local mason = require("mason")
 
-    -- import mason-lspconfig
-    local mason_lspconfig = require("mason-lspconfig")
+		local mason_tool_installer = require("mason-tool-installer")
 
-    local mason_tool_installer = require("mason-tool-installer")
+		-- enable mason and configure icons
+		mason.setup({
+			ui = {
+				icons = {
+					package_installed = "✓",
+					package_pending = "➜",
+					package_uninstalled = "✗",
+				},
+			},
+		})
 
-    -- enable mason and configure icons
-    mason.setup({
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗",
-        },
-      },
-    })
-
-    mason_lspconfig.setup({
-      -- list of servers for mason to install
-      ensure_installed = {
-        "lua_ls",
-        "ruff",
-        "html",
-        "cssls",
-        "helm_ls",
-        "terraformls",
-        "ansiblels",
-      },
-    })
-
-    mason_tool_installer.setup({
-      ensure_installed = {
-        "prettier",
-        "stylua",
-        "yamllint",
-        "yamlfmt",
-        "hclfmt",
-        "helm-ls",
-        "ansible-lint",
-        "nixpkgs-fmt"
-      },
-    })
-  end,
+		mason_tool_installer.setup({
+			ensure_installed = {
+				"lua_ls",
+				"ruff",
+				"helm_ls",
+				"terraformls",
+				"ansiblels",
+				"prettier",
+				"stylua",
+				"yaml-language-server",
+				"yamllint",
+				"yamlfmt",
+				"json-lsp",
+				"jsonlint",
+				"fixjson",
+				"terraform-ls",
+				"tflint",
+				"hclfmt",
+				"helm-ls",
+				"ansible-lint",
+				"nixpkgs-fmt",
+			},
+		})
+	end,
 }
