904498bce593f214313714a68cb5050333279798
Bryan Jones <unix.chkpwd@gmail.com>
2024-10-19 12:39:08 -0400

    removed plugins

--- dot_config/nvim/lua/chkpwd/plugins/kubernetes-nvim.lua
+++
@@ -1,3 +0,0 @@
-return {
-	"diogo464/kubernetes.nvim",
-}
--- dot_config/nvim/lua/chkpwd/plugins/which-key.lua
+++ dot_config/nvim/lua/chkpwd/plugins/which-key.lua
@@ -1,15 +1,11 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
+	"folke/which-key.nvim",
+	event = "VeryLazy",
 
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
+	init = function()
+		vim.o.timeout = true
+		vim.o.timeoutlen = 500
+	end,
 
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  },
+	opts = {},
 }
