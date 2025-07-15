47643384eea8bc3b4a5b33078c8657069c4ed9ca
Bryan Jones <unix.chkpwd@gmail.com>
2025-07-15 15:41:07 -0400

    add work configs

--- dot_zsh_files/utils.zsh
+++ dot_zsh_files/utils.zsh
@@ -24,20 +24,8 @@ function compare_plists() {
     rm "$old" "$new" "$host_old" "$host_new"
 }
 
-
 # transfer.sh alias
 transfer(){ if [ $# -eq 0 ];then echo "No arguments specified.\nUsage:\n transfer <file|directory>\n ... | transfer <file_name>">&2;return 1;fi;if tty -s;then file="$1";file_name=$(basename "$file");if [ ! -e "$file" ];then echo "$file: No such file or directory">&2;return 1;fi;if [ -d "$file" ];then file_name="$file_name.zip" ,;(cd "$file"&&zip -r -q - .)|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null,;else cat "$file"|curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;else file_name=$1;curl --progress-bar --upload-file "-" "https://transfer.sh/$file_name"|tee /dev/null;fi;}
-
-# Enter a running Docker container
-function denter() {
-    if [[ ! "$1" ]] ; then
-        echo "You must supply a container ID or name."
-        return 0
-    fi
-
-    docker exec -it $1 bash
-    return 0
-}
 
 # Password generation
 function pwgen() {
@@ -68,19 +56,9 @@ function cypher {
     sops --encrypt --age $(cat ~/.config/sops/age/keys.txt |grep -oP "public key: \K(.*)") $2 $3 $1 > "$filename.sops.$extension"
 }
 
-function net-utils () {
-  local namespace="$1"
-  cat "$2" | sed -e "s/\$NAMESPACE/$namespace/g" | kubectl apply -f -
-}
-
 function bcrypt () {
   password="$1"
   python -c 'import bcrypt; print(bcrypt.hashpw(b"$password", bcrypt.gensalt(rounds=15)).decode("ascii"))'
-}
-
-function hr-reset () {
-  flux -n $1 suspend hr $2
-  flux -n $1 resume hr $2
 }
 
 function pod-logs () {
